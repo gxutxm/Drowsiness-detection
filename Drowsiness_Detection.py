7d93b6dfdddf45244783e3d66f00064b475aeb3f
Gautam G <gxutxm@gmail.com>
2023-08-28 14:31:09 +0530

    feat: added alert message

--- Drowsiness_Detection.py
+++ Drowsiness_Detection.py
@@ -46,9 +46,9 @@ while True:
 			flag += 1
 			print (flag)
 			if flag >= frame_check:
-				cv2.putText(frame, "****************ALERT!****************", (10, 30),
+				cv2.putText(frame, "****************WAKE UP ALERT!****************", (10, 30),
 					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-				cv2.putText(frame, "****************ALERT!****************", (10,325),
+				cv2.putText(frame, "****************WAKE UP ALERT!****************", (10,325),
 					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 				mixer.music.play()
 		else:
